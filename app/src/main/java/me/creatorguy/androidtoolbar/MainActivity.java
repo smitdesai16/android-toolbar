a54f0fe0df29f41fb66bb5fb537003c0cee3bd7f
Smit Desai <smit.desai16@gmail.com>
2020-06-21 02:31:34 -0500

    Initial commit

---
+++ app/src/main/java/me/creatorguy/androidtoolbar/MainActivity.java
@@ -0,0 +1,41 @@
+package me.creatorguy.androidtoolbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar iToolbar = findViewById(R.id.iToolbar);
+        setSupportActionBar(iToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuMainSettings:
+                Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                break;
+        }
+        return true;
+    }
+}
